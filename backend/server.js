5c83095210962aaf7b178395305291daad26f15b
Saad Qureshi <m.qureshisaad@gmail.com>
2026-04-02 17:16:40 +0530

    Fix helmet CSP blocking inline scripts

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,10 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(helmet());
+app.use(helmet({
+  contentSecurityPolicy: false
+}));
+
 app.use('/api/', rateLimit({ windowMs: 15 * 60 * 1000, max: 100 }));
 app.use(express.static(path.join(__dirname, '../frontend')));
 
